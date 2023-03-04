0ea9600fa551a477fd7ef2f023cde059c4c04ad1
Ildar <mexx0174@gmail.com>
2023-03-04 17:43:28 +0500

    2

---
+++ src/main/java/org/example/dao/UserDaoImpl.java
@@ -0,0 +1,27 @@
+package org.example.dao;
+
+
+import org.example.model.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    public final List<User> users;
+
+    public UserDaoImpl() {
+        users = List.of(
+                new User("Плутоний"),
+                new User("Блесна"));
+    }
+
+
+    @Override
+    public List<User> findAllUsers() {
+        return users;
+    }
+
+    @Override
+    public User getUserByName(String name) {
+        return users.stream().filter(user -> user.getName().equals(name)).findAny().orElse(null);
+    }
+}
